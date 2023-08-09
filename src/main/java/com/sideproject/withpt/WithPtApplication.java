2e121ba3c45167ca41ba0a9101f732faf28aaac4
seyoung5744 <seyoung574458@gmail.com>
2023-08-09 23:41:10 +0900

    init project

---
+++ src/main/java/com/sideproject/withpt/WithPtApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.withpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/withpt/WithPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.withpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
