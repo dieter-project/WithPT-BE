9283ff3838f92bfd0d50e08ce97782d2f5c67756
seyoung5744 <seyoung574458@gmail.com>
2023-08-16 20:33:19 +0900

    feat : @Valid 유효성 검사 및 Enum 타입 유효성 검사

--- src/main/java/com/sideproject/withpt/common/exception/ErrorCode.java
+++ src/main/java/com/sideproject/withpt/common/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    TEST_ERROR(HttpStatus.BAD_REQUEST, "테스트 에러 입니다.");
+    TEST_ERROR(HttpStatus.BAD_REQUEST, "테스트 에러 입니다."),
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네입입니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/sideproject/withpt/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sideproject/withpt/common/exception/GlobalExceptionHandler.java
@@ -2,23 +2,64 @@ package com.sideproject.withpt.common.exception;
 
 import com.sideproject.withpt.common.response.ApiErrorResponse;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(GlobalException.class)
-    protected ResponseEntity<ApiErrorResponse> handleGlobalException(GlobalException e, HttpServletRequest request) {
+    protected ResponseEntity<Object> handleGlobalException(GlobalException e, HttpServletRequest request) {
+        log.warn("handleGlobalException", e);
         return handleExceptionInternal(e.getErrorCode(), request);
     }
 
-    private ResponseEntity<ApiErrorResponse> handleExceptionInternal(ErrorCode errorCode, HttpServletRequest request) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e, HttpServletRequest request) {
+        log.warn("handleIllegalArgument", e);
+        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, e.getMessage(), request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex,
+        HttpHeaders headers,
+        HttpStatus status,
+        WebRequest request)
+    {
+        log.warn("handleMethodArgumentNotValid", ex);
+        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(ex, errorCode, request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, HttpServletRequest request) {
         return ResponseEntity
             .status(errorCode.getHttpStatus())
             .body(ApiErrorResponse.from(errorCode, request));
     }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode, WebRequest request) {
+        return ResponseEntity
+            .status(errorCode.getHttpStatus())
+            .body(ApiErrorResponse.from(e, errorCode, request));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message, HttpServletRequest request) {
+        return ResponseEntity
+            .status(errorCode.getHttpStatus())
+            .body(ApiErrorResponse.from(errorCode, message, request));
+    }
+
+
 
 }
