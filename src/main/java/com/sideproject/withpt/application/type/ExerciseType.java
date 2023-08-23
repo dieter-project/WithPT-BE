37f03c34ec823b005c0d2557faaf56411d1b4425
lazzvery <xxlazvert@gmail.com>
2023-08-23 13:46:03 +0900

    feat(exercise): 운동 기록 입력 구현

--- src/main/java/com/sideproject/withpt/application/type/ExerciseType.java
+++ src/main/java/com/sideproject/withpt/application/type/ExerciseType.java
@@ -5,5 +5,6 @@ import com.sideproject.withpt.common.exception.validator.CustomEnumDeserializer;
 
 @JsonDeserialize(using = CustomEnumDeserializer.class)
 public enum ExerciseType {
+    // 유산소, 무산소, 스트레칭
     AEROBIC, ANAEROBIC, STRETCHING
 }
