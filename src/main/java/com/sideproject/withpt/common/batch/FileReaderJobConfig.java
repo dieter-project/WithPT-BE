4bd1eedae59c6414ac9a9610ed1fef40abffab2f
lazzvery <xxlazvert@gmail.com>
2023-12-01 16:22:17 +0900

    feat(diet): 스프링 배치 재실행 대상 설정 수정

--- src/main/java/com/sideproject/withpt/common/batch/FileReaderJobConfig.java
+++ src/main/java/com/sideproject/withpt/common/batch/FileReaderJobConfig.java
@@ -35,7 +35,6 @@ public class FileReaderJobConfig {
                 .<FoodDto, FoodDto>chunk(chunkSize)
                 .reader(csvReader.csvFoodReader())
                 .writer(csvScheduleWriter)
-                .allowStartIfComplete(true)
                 .build();
     }
 
