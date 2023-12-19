8af6d7703411fedca2ac1166d44fb25eb2755113
seyoung5744 <seyoung574458@gmail.com>
2023-12-19 17:33:00 +0900

    feat(lesson) : 회원 이름 검색

---
+++ src/main/java/com/sideproject/withpt/application/lesson/controller/response/SearchMemberResponse.java
@@ -0,0 +1,31 @@
+package com.sideproject.withpt.application.lesson.controller.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.sideproject.withpt.application.type.Sex;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SearchMemberResponse {
+
+    private Long memberId;
+    private String memberName;
+    private LocalDate birth;
+    private Sex sex;
+    private int remainingPtCount;
+
+    @QueryProjection
+    public SearchMemberResponse(Long memberId, String memberName, LocalDate birth, Sex sex, int remainingPtCount) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.birth = birth;
+        this.sex = sex;
+        this.remainingPtCount = remainingPtCount;
+    }
+}
