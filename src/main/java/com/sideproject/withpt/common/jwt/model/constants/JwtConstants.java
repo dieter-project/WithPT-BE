b213f7b1aa1396395796ca2bfbbce1c1725bb90c
seyoung5744 <seyoung574458@gmail.com>
2024-07-01 15:53:08 +0900

    fix : add JwtConstants

---
+++ src/main/java/com/sideproject/withpt/common/jwt/model/constants/JwtConstants.java
@@ -0,0 +1,15 @@
+package com.sideproject.withpt.common.jwt.model.constants;
+
+import com.sideproject.withpt.application.type.Role;
+
+public interface JwtConstants {
+    String TOKEN_HEADER = "Authorization";
+    String ACCESS_TOKEN_PREFIX = "Bearer ";
+    //    long ACCESS_TOKEN_VALID_TIME = 1000 * 60 * 30; // 30분
+    long ACCESS_TOKEN_VALID_TIME = 1000 * 60 * 60 * 24 * 14;  // 14일
+    long REFRESH_TOKEN_VALID_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    String MEMBER_REFRESH_TOKEN_PREFIX = "Refresh:" + Role.MEMBER;
+    String TRAINER_REFRESH_TOKEN_PREFIX = "Refresh:" + Role.TRAINER;
+    String ACCESS_TOKEN_BLACK_LIST_PREFIX = "Black-Access:";
+
+}
