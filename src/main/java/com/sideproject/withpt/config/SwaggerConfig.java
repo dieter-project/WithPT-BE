c3637e583e9af7bcc64b714b09b324192658bcfa
seyoung5744 <seyoung574458@gmail.com>
2024-07-03 13:31:06 +0900

    feat(swagger) : api 그룹별로 문서 분리

--- src/main/java/com/sideproject/withpt/config/SwaggerConfig.java
+++ src/main/java/com/sideproject/withpt/config/SwaggerConfig.java
@@ -2,6 +2,11 @@ package com.sideproject.withpt.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,14 +22,74 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        String[] paths = {"/api/v1/**"};
-
+    public GroupedOpenApi memberApi() {
         return GroupedOpenApi.builder()
-            .group("WithPT-API v1")
-            .pathsToMatch(paths)
+            .group("member")
+            .pathsToMatch("/api/v1/members/**")
             .build();
     }
 
+    @Bean
+    public GroupedOpenApi trainerApi() {
+        return GroupedOpenApi.builder()
+            .group("trainer")
+            .pathsToMatch("/api/v1/trainers/**")
+            .build();
+    }
 
+    @Bean
+    public GroupedOpenApi oauthApi() {
+        return GroupedOpenApi.builder()
+            .group("oauth")
+            .pathsToMatch("/api/v1/oauth/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi gymApi() {
+        return GroupedOpenApi.builder()
+            .group("gym")
+            .pathsToMatch("/api/v1/gyms/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi lessonApi() {
+        return GroupedOpenApi.builder()
+            .group("lesson")
+            .pathsToMatch("/api/v1/lessons/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi chatApi() {
+        return GroupedOpenApi.builder()
+            .group("chat")
+            .pathsToMatch("/api/v1/chat/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder()
+            .group("all")
+            .pathsToMatch("/api/v1/**")
+            .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("JWT"))
+            .components(new Components()
+                .addSecuritySchemes("JWT", createAPIKeyScheme())
+            );
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+            .type(Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("Bearer");
+    }
 }
