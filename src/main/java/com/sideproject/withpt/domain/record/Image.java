a18546f1ee39d6d16725b217f88a92c783d42fc1
seyoung5744 <seyoung574458@gmail.com>
2024-10-13 23:29:57 +0900

    fix : image 도메인 불필요한 필드 삭제

--- src/main/java/com/sideproject/withpt/domain/record/Image.java
+++ src/main/java/com/sideproject/withpt/domain/record/Image.java
@@ -4,18 +4,28 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sideproject.withpt.common.type.Usages;
 import com.sideproject.withpt.domain.BaseEntity;
 import com.sideproject.withpt.domain.member.Member;
-import lombok.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.*;
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 public class Image extends BaseEntity {
 
     @Id
@@ -39,9 +49,5 @@ public class Image extends BaseEntity {
     private String uploadUrlPath;
 
     private String attachType;
-
-//    @CreatedDate
-//    @Column(updatable = false, nullable = false)
-//    private LocalDateTime createdDate;
 
 }
